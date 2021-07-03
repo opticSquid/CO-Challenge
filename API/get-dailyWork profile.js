5f5f49ceb79ecfc290e930997c8293a8e6731ac6
Soumalya Bhattacharya <59192460+soumalyatheking22012001@users.noreply.github.com>
2021-07-03 11:35:08 +0530

    Added few routes to make getting data easier

--- API/get-dailyWork profile.js
+++ API/get-dailyWork profile.js
@@ -1,30 +1,28 @@
 const router = require("express").Router();
 const userAuth = require("../Middleware/UserAuth");
 const workProfile = require("../Database/work-profile");
-const non_workProfile = require("../Database/non-work-profile");
 const getSchedule = async (req, res, next) => {
   let day = req.params.day;
   console.log("Day=> ", day);
   try {
     let wSchedule = await workProfile.FindSchedule(res.locals.user.Email, day);
-    let NwSchedule = await non_workProfile.FindSchedule(
-      res.locals.user.Email,
-      day
-    );
     console.log("Find work schedule query executed", wSchedule);
-    console.log("Find non-work schedule query executed", NwSchedule);
-    let schedule = {
-      work_profile: wSchedule.data,
-      non_work_profile: NwSchedule.data,
-    };
     res
       .status(200)
-      .json({ response: "Found the schedule of your day", day: day, data: schedule });
+      .json({
+        response: "Found the work schedule for the day",
+        day: day,
+        data: wSchedule.data,
+      });
   } catch (e) {
     console.error("Find schedule query returned error: ", e);
     res
       .status(500)
-      .json({ response: "Could not find your schedule", day: day, data: null });
+      .json({
+        response: "Could not find your work schedule",
+        day: day,
+        data: null,
+      });
   }
 };
 router.get(
--- Routes/routes.js
+++ Routes/routes.js
@@ -3,5 +3,5 @@ const router = require("express").Router();
 router.use("/", require("../API/home"));
 router.use("/user",require("./user"));
 router.use("/app-profiles",require("./profiles"));
-
+router.use("/schedules",require("./schedules"));
 module.exports = router;
